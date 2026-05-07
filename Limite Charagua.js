394851ce916cd291b3e6e4a9c6e5c975fb9d4b67
Gerartemisa <april.gerartemisa@gmail.com>
2026-05-06 23:53:12 -0400

    Delete Limite Charagua.js

--- Limite Charagua.js
+++
@@ -1,7 +0,0 @@
-// Limite de Charagua
-// Limite 
-var a = ee.FeatureCollection('projects/ee-mrv-char/assets/CHAR')
-// Limite con area de influencia 
-var b = ee.FeatureCollection("projects/ee-mrv-char/assets/CHAR_buffer")
-Map.addLayer(a,{color: 'FF0000'},'Limite Charagua')
-Map.addLayer(b,{color: '000000'},'Limite Charagua Buffer')
